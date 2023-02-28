a0decae705cc0ca1531a15e6d25d6c1e0859ace8
Tatiana Osipova <otv120780@gmail.com>
2023-02-28 16:52:53 -0500

    feat: add animation to trees

--- game.js
+++ game.js
@@ -1,16 +1,41 @@
-
-
 (function game() {
     let isPause = false;
+    let animationId = null;
+
+    const speed = 3;
 
     const car = document.querySelector('.car');
     const trees = document.querySelectorAll('.tree');
 
-    const gameButton = document.querySelector('.game-button');
+    const tree1 = trees[0];    
 
+    animationId = requestAnimationFrame(startGame);
+
+    function startGame() {
+        treesAnimation();
+
+        animationId =requestAnimationFrame(startGame);
+    } 
+    
+    function treesAnimation() {
+        const newCoord = getYCoord(tree1) + speed;
+        tree1.style.transform = `translateY(${newCoord}px)`;
+    }
+    
+    function getYCoord(element) {
+        const matrix = window.getComputedStyle(element).transform;
+        const array = matrix.split(',');
+        const lastElement = array[array.length - 1];
+        const coordY = parseFloat(lastElement);
+
+        return coordY;
+    }
+
+    const gameButton = document.querySelector('.game-button');
     gameButton.addEventListener('click', () => {
         isPause = !isPause;
         if (isPause) {
+            cancelAnimationFrame(animationId);
             gameButton.children[0].style.display = 'none';
             gameButton.children[1].style.display = 'initial';
         }
@@ -19,8 +44,4 @@
             gameButton.children[1].style.display = 'none';
         }
     });
-
-    console.log(car);
-    console.log(trees);
-
 })();

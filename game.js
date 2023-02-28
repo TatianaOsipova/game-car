fb1a47c2d210c6c9590a97e9457f6e77c256ab9e
Tatiana Osipova <otv120780@gmail.com>
2023-02-28 16:13:46 -0500

    feat: change image to button play

--- game.js
+++ game.js
@@ -1,13 +1,23 @@
 
 
 (function game() {
+    let isPause = false;
+
     const car = document.querySelector('.car');
     const trees = document.querySelectorAll('.tree');
 
     const gameButton = document.querySelector('.game-button');
 
     gameButton.addEventListener('click', () => {
-
+        isPause = !isPause;
+        if (isPause) {
+            gameButton.children[0].style.display = 'none';
+            gameButton.children[1].style.display = 'initial';
+        }
+        else{
+            gameButton.children[0].style.display = 'initial';
+            gameButton.children[1].style.display = 'none';
+        }
     });
 
     console.log(car);

ce75e9aeb31a98dad245cbc55a886af55969d3b2
Tatiana Osipova <otv120780@gmail.com>
2023-02-28 15:32:22 -0500

    feat: start js

--- game.js
+++ game.js
@@ -1,1 +1,12 @@
-console.log('test');
+
+
+(function game() {
+    const car = document.querySelector('.car');
+    const trees = document.querySelectorAll('.tree');
+
+    const gameButton = document.querySelector
+
+    console.log(car);
+    console.log(trees);
+
+})();

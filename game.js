2e20b6b2e31d5c0dbd0a5f0b180a9c26cc9dc2b1
Tatiana Osipova <otv120780@gmail.com>
2023-02-28 15:35:17 -0500

    feat: add extra classes and image to button

--- game.js
+++ game.js
@@ -4,7 +4,11 @@
     const car = document.querySelector('.car');
     const trees = document.querySelectorAll('.tree');
 
-    const gameButton = document.querySelector
+    const gameButton = document.querySelector('.game-button');
+
+    gameButton.addEventListener('click', () => {
+
+    });
 
     console.log(car);
     console.log(trees);
